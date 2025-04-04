5dd8229a9d6e0c36b456d5512990b7b225cf68d8
Guxtavoc <gustavocurado1234@gmail.com>
2025-04-04 18:47:23 -0400

    Exercicios 2 a 4

--- src/Conta.java
+++ src/Conta.java
@@ -1,22 +1,22 @@
 
 public class Conta {
-	String nome,dataAbertura;
-	int numeroConta, agencia;
-	double saldo;
+	String nome,agencia,dataAbertura;
+	int numeroConta = 0;
+	double saldo = 0;
 	public void imprimeCadastro() {
-		System.out.printf("Nome do cliente: %s    Cliente desde: %s\nNúmero da conta: %d    Agência: %d\n",nome,dataAbertura,numeroConta,agencia);
-		System.out.printf("Saldo atual: %.2f\n",saldo);
+		System.out.printf("Nome do cliente: %s    Cliente desde: %s\nNúmero da conta: %d    Agência: %s\n",this.nome,this.dataAbertura,this.numeroConta,this.agencia);
+		System.out.printf("Saldo atual: %.2f\n",this.saldo);
 	}
 	public void saque(double v) {
-		saldo-=v;
+		this.saldo-=v;
 	}
 	public void deposito(double v) {
-		saldo+=v;
+		this.saldo+=v;
 	}
 	public void mostraSaldo() {
-		System.out.println(saldo);
+		System.out.println(this.saldo);
 	}
 	public void calculaRendimento() {
-		saldo*=1.1;
+		this.saldo*=1.1;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,36 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 		Conta f1 = new Conta();
 		f1.nome="Gustavo";
-		f1.agencia=123;
+		f1.agencia="Banco SulAmericano";
 		f1.numeroConta=456;
 		f1.saldo=0f;
-		f1.dataAbertura="02/04/2025";
-		f1.imprimeCadastro();
-		f1.deposito(50f);
+		f1.dataAbertura="12/12/2000";
+		f1.deposito(50f);//exercicio 2
+		f1.mostraSaldo();
+		f1.saque(45.2);//exercicio 2
 		f1.mostraSaldo();
 		f1.calculaRendimento();
 		f1.mostraSaldo();
+		f1.imprimeCadastro();//Exercicio 3
+		Conta c1 = new Conta();
+		Conta c2 = new Conta();
+		c1.nome = "Gustavo";
+		c1.saldo = 100;
+		c2.nome = "Gustavo";
+		c2.saldo = 100;
+		if(c1==c2) {
+			System.out.println("São iguais!");
+		}
+		else{System.out.println("Não são iguais!");//Exercicio 4
+		}
+		Conta c3 = new Conta();
+		c3=c1;
+		if(c1==c3) {
+			System.out.println("São iguais!");
+		}
+		else{System.out.println("Não são iguais!");//Exercicio 5
+		}
 	}
-
 }
